e1ec3e0b02e9e99c10e7178b811817b76301a696
SheriffKoder <sheriff.koder@hotmail.com>
2022-09-03 23:58:43 +0200

    Recap progress

---
+++ REC09.js
@@ -0,0 +1,17 @@
+
+
+function selection_function (x) {
+
+    console.log(x.value);
+
+}
+
+function FUN () {
+
+
+    let y = myform.input_name0.value;  //named myform
+	myform.input_name1.focus();
+
+    console.log(y);
+
+}
