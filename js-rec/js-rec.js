2f8092da2e3eaf13b2e7969aeb5e757bc54b770e
SheriffKoder <sheriff.koder@hotmail.com>
2022-12-12 17:19:23 +0200

    Recap 09

--- js-rec/js-rec.js
+++ js-rec/js-rec.js
@@ -11,3 +11,28 @@
 
 
 */
+
+function Dog (p) {
+    this.place = p;
+}
+
+
+function ShowDog(name, breed, weight, handler) {
+    this.name = name;
+    this.breed = breed;
+    this.weight = weight;
+    this.handler = handler;
+   }
+
+   ShowDog.prototype = new Dog();   //or aDog;
+   //ShowDog.prototype.constructor = ShowDog; explained below
+   ShowDog.prototype.league = "Webville";
+   ShowDog.prototype.stack = function() {
+        console.log("Stack");
+    };
+
+
+let fido = new ShowDog("fido2", "german", "12", "admin");
+console.log("hi");
+fido.place = "nordic";
+console.log(fido.place);
